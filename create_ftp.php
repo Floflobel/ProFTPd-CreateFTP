bf308d87fe8528b0e28f50a9277662d00854f42d
Florian Bellencontre <fbellencontre@cosium.com>
2019-11-06 12:17:45 +0100

    bu

--- create_ftp.php
+++ create_ftp.php
@@ -17,33 +17,21 @@ global $cfg;
 
 $ac = new AdminClass($cfg);
 
-$field_userid   = $cfg['field_userid'];
 $field_uid      = $cfg['field_uid'];
-$field_ugid     = $cfg['field_ugid'];
-$field_ad_gid   = 'ad_gid';
+$field_login	= $cfg['field_login'];
+$field_ftpname	= $cfg['field_ftpname'];
 $field_passwd   = $cfg['field_passwd'];
-$field_homedir  = $cfg['field_homedir'];
+$field_path	= $cfg['field_path'];
 $field_shell    = $cfg['field_shell'];
-$field_name     = $cfg['field_name'];
-$field_company  = $cfg['field_company'];
-$field_email    = $cfg['field_email'];
-$field_comment  = $cfg['field_comment'];
-$field_disabled = $cfg['field_disabled'];
-
-$groups = $ac->get_groups();
-
-if (count($groups) == 0) {
-  $errormsg = 'There are no groups in the database; please create at least one group before creating users.';
-}
 
 /* Data validation */
 if (empty($errormsg) && !empty($_REQUEST["action"]) && $_REQUEST["action"] == "create") {
   $errors = array();
   /* user id validation */
-  if (empty($_REQUEST[$field_userid])
-      || !preg_match($cfg['userid_regex'], $_REQUEST[$field_userid])
-      || strlen($_REQUEST[$field_userid]) > $cfg['max_userid_length']) {
-    array_push($errors, 'Invalid user name; user name must contain only letters, numbers, hyphens, and underscores with a maximum of '.$cfg['max_userid_length'].' characters.');
+  if (empty($_REQUEST[$field_ftpname])
+      || !preg_match($cfg['ftpname_regex'], $_REQUEST[$field_ftpname])
+      || strlen($_REQUEST[$field_ftpname]) > $cfg['max_ftpname_length']) {
+    array_push($errors, 'Invalid user name; user name must contain only letters, numbers, hyphens, and underscores with a maximum of '.$cfg['max_ftpname_length'].' characters.');
   }
   /* uid validation */
   if (empty($cfg['default_uid']) || !$ac->is_valid_id($cfg['default_uid'])) {
@@ -58,62 +46,31 @@ if (empty($errormsg) && !empty($_REQUEST["action"]) && $_REQUEST["action"] == "c
   } else if ($cfg['min_uid'] != -1 && $cfg['default_uid'] < $cfg['min_uid']) {
     array_push($errors, 'Invalid UID; UID must be at least ' . $cfg['min_uid'] . '.');
   }
-  /* gid validation */
-  if (empty($_REQUEST[$field_ugid]) || !$ac->is_valid_id($_REQUEST[$field_ugid])) {
-    array_push($errors, 'Invalid main group; GID must be a positive integer.');
-  }
   /* password length validation */
   if (strlen($_REQUEST[$field_passwd]) < $cfg['min_passwd_length']) {
     array_push($errors, 'Password is too short; minimum length is '.$cfg['min_passwd_length'].' characters.');
   }
-//  if (strlen($_REQUEST[$field_homedir]) <= 1) {
-//    array_push($errors, 'Invalid home directory; home directory cannot be empty.');
-//  }
   /* shell validation */
   if (strlen($cfg['default_shell']) <= 1) {
     array_push($errors, 'Invalid shell; shell cannot be empty.');
   }
   /* user name uniqueness validation */
-  if ($ac->check_username($_REQUEST[$field_userid])) {
+  if ($ac->check_username($_REQUEST[$field_ftpname])) {
     array_push($errors, 'User name already exists; name must be unique.');
-  }
-  /* gid existance validation */
-  if (!$ac->check_gid($_REQUEST[$field_ugid])) {
-    array_push($errors, 'Main group does not exist; GID cannot be found in the database.');
   }
   /* data validation passed */
   if (count($errors) == 0) {
-    while (list($g_gid, $g_group) = each($groups)) { 
-      if($_REQUEST[$field_ugid] == $g_gid) {
-        $name_group = $g_group;
-      }
-    }
     $disabled = isset($_REQUEST[$field_disabled]) ? '1':'0';
-    $userdata = array($field_userid   => $_REQUEST[$field_userid],
-                      $field_uid      => $cfg['default_uid'],
-                      $field_ugid     => $_REQUEST[$field_ugid],
+    $userdata = array($field_uid      => $cfg['default_uid'],
+		      $field_login    => $_SERVER['PHP_AUTH_USER'], 
+                      $field_ftpname   => $_REQUEST[$field_ftpname],
                       $field_passwd   => $_REQUEST[$field_passwd],
-                      $field_homedir  => $cfg['default_homedir'] . $name_group . "/" . $_REQUEST[$field_userid],
-                      $field_shell    => $cfg['default_shell'],
-                      $field_name     => $_REQUEST[$field_name],
-                      $field_email    => $_REQUEST[$field_email],
-                      $field_company  => $_REQUEST[$field_company],
-                      $field_comment  => $_REQUEST[$field_comment],
-                      $field_disabled => $disabled);
+                      $field_path  => $cfg['default_path'] . $_REQUEST[$field_ftpname],
+                      $field_shell    => $cfg['default_shell']);
     if ($ac->add_user($userdata)) {
-      if (isset($_REQUEST[$field_ad_gid])) {
-        while (list($g_key, $g_gid) = each($_REQUEST[$field_ad_gid])) {
-          if (!$ac->is_valid_id($g_gid)) {
-            $warnmsg = 'Adding additional group failed; at least one of the additional groups had an invalid GID.';
-            continue;
-          }
-          // XXX: fix error handling here
-          $ac->add_user_to_group($_REQUEST[$field_userid], $g_gid);
-        }
-      }
-      $infomsg = 'User "'.$_REQUEST[$field_userid].'" created successfully.';
+      $infomsg = 'User "'.$_REQUEST[$field_ftpname].'" created successfully.';
     } else {
-      $errormsg = 'User "'.$_REQUEST[$field_userid].'" creation failed; check log files.';
+      $errormsg = 'User "'.$_REQUEST[$field_ftpname].'" creation failed; check log files.';
     }
   } else {
     $errormsg = implode($errors, "<br />\n");
@@ -123,42 +80,27 @@ if (empty($errormsg) && !empty($_REQUEST["action"]) && $_REQUEST["action"] == "c
 /* Form values */
 if (isset($errormsg)) {
   /* This is a failed attempt */
-  $userid   = $_REQUEST[$field_userid];
   $uid      = $cfg['default_uid'];
-  $ugid     = $_REQUEST[$field_ugid];
-  $ad_gid   = $_REQUEST[$field_ad_gid];
+  $login  = $_REQUEST[$field_login];
+  $ftpname   = $_REQUEST[$field_ftpname];
   $passwd   = $_REQUEST[$field_passwd];
-  $homedir  = $cfg['default_homedir'];
+  $path  = $cfg['default_path'];
   $shell    = $cfg['default_shell'];
-  $name     = $_REQUEST[$field_name];
-  $email    = $_REQUEST[$field_email];
-  $company  = $_REQUEST[$field_company];
-  $comment  = $_REQUEST[$field_comment];
-  $disabled = isset($_REQUEST[$field_disabled]) ? '1' : '0';
 } else {
   /* Default values */
-  $userid   = "";
+  $ftpname   = "";
   if (empty($cfg['default_uid'])) {
     $uid    = $ac->get_last_uid() + 1;
   } else {
     $uid    = $cfg['default_uid'];
   }
   if (empty($infomsg)) {
-    $ugid   = "";
-    $ad_gid = array();
     $shell  = "/bin/false";
   } else {
-    $ugid    = $_REQUEST[$field_ugid];
-    $ad_gid = @$_REQUEST[$field_ad_gid];
     $shell  = $cfg['default_shell'];
   }
   $passwd   = $ac->generate_random_string((int) $cfg['default_passwd_length']);
-  $homedir  = $cfg['default_homedir'];
-  $name     = "";
-  $email    = "";
-  $company  = "";
-  $comment  = "";
-  $disabled = '0';
+  $path  = $cfg['default_path'];
 }
 
 include ("includes/header.php");
@@ -174,13 +116,6 @@ include ("includes/header.php");
       <div class="row">
         <div class="col-sm-12">
           <form role="form" class="form-horizontal" method="post" data-toggle="validator">
-	    <!-- Login -->
-            <div class="form-group">
-              <label for="<?php echo $field_login; ?>" class="col-sm-4 control-label">You are</label>
-              <div class="controls col-sm-8">
-                <input type="text" class="form-control" id="<?php echo $field_login; ?>" name="<?php echo $field_login; ?>" value="<?php echo $_SERVER['PHP_AUTH_USER'] ?>" required disabled />
-              </div>
-            </div>
             <!-- FTP name -->
             <div class="form-group">
               <label for="<?php echo $field_ftpname; ?>" class="col-sm-4 control-label">FTP name <font color="red">*</font></label>
@@ -194,20 +129,6 @@ include ("includes/header.php");
               <label for="<?php echo $field_passwd; ?>" class="col-sm-4 control-label">Password</label>
               <div class="controls col-sm-8">
                 <input type="text" class="form-control" id="<?php echo $field_passwd; ?>" name="<?php echo $field_passwd; ?>" value="<?php echo $passwd; ?>" placeholder="Mandatory password" minlength="<?php echo $cfg['min_passwd_length']; ?>" required disabled />
-              </div>
-            </div>
-            <!-- Path -->
-            <div class="form-group">
-              <label for="<?php echo $field_name; ?>" class="col-sm-4 control-label">Path</label>
-              <div class="controls col-sm-8">
-                <input type="text" class="form-control" id="<?php echo $field_name; ?>" name="<?php echo $field_name; ?>" value="<?php echo $name; ?>" placeholder="" required disabled />
-              </div>
-            </div>
-            <!-- Ticket number -->
-            <div class="form-group">
-              <label for="<?php echo $field_name; ?>" class="col-sm-4 control-label">Ticket number<font color="red">*</font></label>
-              <div class="controls col-sm-8">
-                <input type="text" class="form-control" id="<?php echo $field_name; ?>" name="<?php echo $field_name; ?>" value="<?php echo $name; ?>" placeholder="XX-XXX" required />
               </div>
             </div>
             <!-- Actions -->
--- tables.sql
+++ tables.sql
@@ -1,32 +1,14 @@
 #
-# Table structure for table `groups`
-#
-
-CREATE TABLE `groups` (
-  `groupname` varchar(32) NOT NULL default '',
-  `gid` smallint(6) unsigned NOT NULL auto_increment,
-  `members` varchar(255) NOT NULL default '',
-  PRIMARY KEY  (`gid`),
-  UNIQUE KEY `groupname` (`groupname`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci COMMENT='ProFTPd group table';
-
-#
 # Table structure for table `users`
 #
 
 CREATE TABLE `users` (
   `id` smallint(2) unsigned NOT NULL auto_increment,
-  `userid` varchar(32) NOT NULL default '',
+  `login` varchar(32) NOT NULL default '',
   `uid` smallint(6) unsigned default NULL,
-  `gid` smallint(6) unsigned default NULL,
   `passwd` varchar(265) NOT NULL default '',
-  `homedir` varchar(255) NOT NULL default '',
-  `comment` varchar(255) NOT NULL default '',
-  `disabled` smallint(2) unsigned NOT NULL default '0',
+  `path` varchar(255) NOT NULL default '',
   `shell` varchar(32) NOT NULL default '/sbin/nologin',
-  `email` varchar(255) NOT NULL default '',
-  `name` varchar(255) NOT NULL default '',
-  `company` varchar(255) NOT NULL default '',
   `bytes_in_used` bigint(20) unsigned NOT NULL default '0',
   `bytes_out_used` bigint(20) unsigned NOT NULL default '0',
   `files_in_used` bigint(20) unsigned NOT NULL default '0',
