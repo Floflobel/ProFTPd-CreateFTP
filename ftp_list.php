4d978ef1b449b5b0905fd6ed01b1b5148e9b0a02
Florian Bellencontre <fbellencontre@cosium.com>
2019-11-06 15:33:41 +0100

    bu

--- ftp_list.php
+++ ftp_list.php
@@ -146,7 +146,6 @@ include ("includes/header.php");
               <tbody>
                 <?php foreach ($users as $user) { ?>
                   <tr>
-
                     <td class="pull-middle"><?php echo $user[$field_uid]; ?></td>
                     <td class="pull-middle"><a href="edit_ftp.php?action=show&<?php echo $field_id; ?>=<?php echo $user[$field_id]; ?>"><?php echo $user[$field_userid]; ?></a></td>
                     <td class="pull-middle"><?php echo $all_groups[$user[$field_ugid]]; ?></td>
