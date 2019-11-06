b8a3f8479f8c850ebd055cb60499895447c27926
Florian Bellencontre <fbellencontre@cosium.com>
2019-11-06 17:30:00 +0100

    bu

--- includes/AdminClass.php
+++ includes/AdminClass.php
@@ -576,10 +576,11 @@ class AdminClass {
           $passwd_query = sprintf($passwd_format, $field_passwd, $passwd);
         }
 
-        $format = 'UPDATE %s SET %s WHERE %s=%s';
+        $format = 'UPDATE %s SET %s WHERE %s="%s"';
         $query = sprintf($format, $this->config['table_users'],
                                   $passwd_query,
                                   $field_id,       $userdata[$field_id]);
+	$print_r($query);
         $result = $this->dbConn->query($query);
         return $result;
     }
