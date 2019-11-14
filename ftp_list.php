d37c1fbd355eb849e227019162c9551655ee92bf
Florian Bellencontre <fbellencontre@cosium.com>
2019-11-14 14:44:41 +0100

    bu

--- ftp_list.php
+++ ftp_list.php
@@ -38,10 +38,10 @@ $users = array();
 
 /* return SFTP name and password */
 if (isset($_GET["create_userid"]) && isset($_GET["create_password"])) {
-  $infomsg = $cfg['create_custom_message']
+  $infomsg = $cfg['create_custom_message'];
 }
 if (isset($_GET["update_userid"]) && isset($_GET["update_password"])) {
-  $infomsg = $cfg['update_custom_message']
+  $infomsg = $cfg['update_custom_message'];
 }
 
 /* parse filter  */
